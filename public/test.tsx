f5d5d55a7a298b0d522a3d1d72fca964a0da9fe8
septavian7 <134582182+septavian7@users.noreply.github.com>
2024-03-24 17:30:24 -0700

    hoverbarnew3: fixed test.tsx

--- public/test.tsx
+++ public/test.tsx
@@ -2,17 +2,19 @@
 
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { ThemeProvider } from "../src/providers/SkThemeProvider";
-import FramerMotionTest from "../src/components/layout/SkHoverBarNew/SkHoverBarNew";
+import { ThemeProvider } from "@emotion/react";
+import { getThemeStyles } from "../src/styles/SkThemeStyles";
+import SkHoverBarNew3 from "../src/components/layout/SkHoverBarNew3/SkHoverBarNew3";
 
 const rootElement = document.getElementById("test-root");
 if (rootElement) {
   const root = createRoot(rootElement);
+
+  const theme = getThemeStyles("light");
+
   root.render(
-    <React.StrictMode>
-      <ThemeProvider>
-        <FramerMotionTest />
-      </ThemeProvider>
-    </React.StrictMode>,
+    <ThemeProvider theme={theme}>
+      <SkHoverBarNew3 />
+    </ThemeProvider>,
   );
 }
